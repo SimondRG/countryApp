9d2ce903e292b798210c81693257459432bcb7f3
Simon Rojas <sdavid.rojas@hotmail.com>
2024-05-08 22:39:27 -0500

    Creación, ajuste y maquetado del componente countryTable en el modulo de countries para mostrar la información de las capitales en pantalla

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -11,7 +11,9 @@ export class CountriesService {
     constructor( private http: HttpClient ) { }
 
     searchCapital( term: string ): Observable<Country[]>{
+
         const url = `${this.apiUrl}/capital/${ term }`;
+        
         return this.http.get<Country[]>( url );
     }
     
