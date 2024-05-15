a14e6238c870d9c121101cf26b5d9263c951aeea
Simon Rojas <sdavid.rojas@hotmail.com>
2024-05-15 14:32:25 -0500

    Se ajusto el componente de country-page para mostrar el detalle de país, con los parametros de la ruta, y switchMap para el manejo eficiente de los obervables

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
+
+  constructor( 
+    private activateRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ) )
+      )
+      .subscribe( ( country ) =>{
+        this.navigateHome( country );
+      });
+  }
+
+  navigateHome( country: Country | null ) {
+    if ( !country ) return this.router.navigateByUrl('');
+    console.log( { respuesta: country} );
+    return this.country = country;    
+    
+  }
 
 }
