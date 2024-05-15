714cffa9f60842982438c89d3dc709aaa07a2e7f
Simon Rojas <sdavid.rojas@hotmail.com>
2024-05-14 21:21:48 -0500

    Se ajusto el servicio con CatchError en el observable que devulve el servicio en caso que exista una consulta invalida de la petición a la API, con manejo de Rxjs

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -14,7 +14,10 @@ export class CountriesService {
 
         const url = `${this.apiUrl}/capital/${ term }`;
         
-        return this.http.get<Country[]>( url );
+        return this.http.get<Country[]>( url )
+        .pipe(
+            catchError( () => of([]) )
+        );
     }
     
 }
