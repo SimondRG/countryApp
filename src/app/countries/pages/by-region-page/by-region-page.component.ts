fb6949578531f120c4c5927e123ac62d882b41b1
Simon Rojas <sdavid.rojas@hotmail.com>
2024-05-17 09:11:19 -0500

    Se ajusto el componente de by-region para que solo se puedan elegir los continentes que el api proporciona

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -13,8 +16,12 @@ export class ByRegionPageComponent {
 
   public region: Country[] = []
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa' , 'Americas' , 'Asia' , 'Europe' , 'Oceania'];
+  public selectedRegion?: Region;
 
-  searchByRegion(region: string): void{
+  searchByRegion(region: Region): void{
+
+    this.selectedRegion = region;
 
     this.isLoading = true;
 
